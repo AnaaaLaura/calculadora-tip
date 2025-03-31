d004d1f41013102fd78cc2e0c977b88464d7b360
larissa <analauracarvalho3991@gmail.com>
2025-03-31 20:29:13 -0300

    clicou selecionou

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let bill = 0
-let tip = 0
+let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
-let billInput = document.querySelector("#bill")
-billInput.addEventListener("input", () => {
-    bill = billInput.valeuAsNumber
 
-    console.log(bill)
-})
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
 
-console.log(billInput)
+}
+
+function receiveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+
+}
+
+function receiveTipPercentageValue(value){
+    tipPercentage = value / 100
+
+    if(buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+}
