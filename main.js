89d6a4a8a6e66614069464c0b0bbd92f4d093197
larissa <analauracarvalho3991@gmail.com>
2025-03-31 18:40:46 -0300

    inicio da javas

---
+++ main.js
@@ -0,0 +1,12 @@
+let bill = 0
+let tip = 0
+let numberOfPeople = 0
+
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", () => {
+    bill = billInput.valeuAsNumber
+
+    console.log(bill)
+})
+
+console.log(billInput)
